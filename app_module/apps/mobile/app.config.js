0598701682b18bb7b4e44972211a0cf5c2466b56
centervalentine <centervalentine@gmail.com>
2025-10-15 15:20:13 -0600

    expo simulation troubleshooting

--- app_module/apps/mobile/app.config.js
+++ app_module/apps/mobile/app.config.js
@@ -1,5 +1,8 @@
 // NOTE: Environment variable names must not include hyphens when accessed via dot notation (use underscores).
-try { require('dotenv').config(); } catch {}
+// try { require('dotenv').config(); } catch {}
+import dotenv from 'dotenv'
+dotenv.config({ path: '../../.env' }) // load root
+dotenv.config()                      // then local (overrides)
 
 // Use one “non-specific” fallback for both platforms
 // app.config.js
@@ -11,13 +14,19 @@ export default {
     version: "1.0.0",
     "owner": "centervalentine",
     orientation: "portrait",
-    sdkVersion: "51.0.0",
+sdkVersion: "52.0.0",
+    
+plugins: [],
+
     ios: { supportsTablet: true,
        config: {
         // eas secret:create --name xyz --value<value>
         // These will populate process.env.* during the build, app.config.js reads
         googleMapsApiKey: process.env.IOS_GOOGLE_MAPS_API_KEY ?? process.env.NON_SPECIFIC_GOOGLE_MAPS_API_KEY
       },
+      "infoPlist": {
+      "ITSAppUsesNonExemptEncryption": false
+    },
       icon: "./assets/adaptive-icon.png",
       bundleIdentifier: "com.dpv.gardenplot",
     
